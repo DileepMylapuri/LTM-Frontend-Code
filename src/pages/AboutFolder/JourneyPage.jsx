58f4da25869270d43309042bf33db5b01955a906
DileepLee <mylapuridileep@gmail.com>
2026-03-31 21:48:32 +0530

    Update the code structure design

--- src/pages/AboutFolder/JourneyPage.jsx
+++ src/pages/AboutFolder/JourneyPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-const API = "http://localhost:5000/api/journey";
+const API = "https://ltm-frontend-code.onrender.com/api/journey";
 
 const JourneyPage = () => {
   const { type } = useParams();
